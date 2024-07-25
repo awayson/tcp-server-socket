514773d0e9e3ec0e032aacc0210fe8325a89005e
Andrew <andrewbwayson@gmail.com>
2024-07-25 00:55:53 -0400

    Restructure

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+
+string server = "127.0.0.1";
+int port = 8080;
+
+try
+{
+	TcpClient client = new TcpClient(server, port);
+	
+	NetworkStream stream = client.GetStream();
+	
+	string message = "ping";
+	byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+	stream.Write(messageBytes, 0, messageBytes.Length);
+	Console.WriteLine("Sent: {0}", message);
+
+	byte[] response = new byte[4];
+	stream.Read(response, 0, response.Length);
+	string responseMessage = Encoding.ASCII.GetString(response);
+	Console.WriteLine("Received: {0}", responseMessage);
+	
+	client.Close();
+}
+catch (Exception e)
+{
+	Console.WriteLine("Exception: {0}", e.Message);
+}
+
--- Server/obj/Debug/net8.0/TCPServer.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/TCPServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75635752e4f5a6d0530e8e31a3cdad62fba97273")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076cf09cca429e25a8d362d6901085ffe423a9eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
